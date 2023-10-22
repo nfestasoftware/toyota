02b10ab6399c5ed46cf97258ce248d334e484ea6
nfestasoftware <147266616+nfestasoftware@users.noreply.github.com>
2023-10-22 09:48:36 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 echo app ready for deployment
-app deployment1 ready
-added deployment2 ready
-adding deployment3 ready line
-adding deployment4 line
-testing git merge and rebase
-adding a second test merge and rebase line
-adding a third line of test
-testing git cherry-pick
-adding second line for testing cherry-pick
+echo app deployment1 ready
+echo added deployment2 ready
+echo adding deployment3 ready line
+echo adding deployment4 line
+echo testing git merge and rebase
+echo adding a second test merge and rebase line
+echo adding a third line of test
+echo testing git cherry-pick
+echo adding second line for testing cherry-pick
