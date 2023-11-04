2dc178fcc7bd3a1c60fbff6d8bcf62dcbc174206
nfestasoftware <nfestasoftware@gmail.com>
2023-11-04 10:30:57 -0400

    adding a test file

---
+++ test_completion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo 'we are a great testing day'
+echo 'hope you are enjoying along with us'
+sleep 5
+echo 'testing in progress and will keep you post shortly..'
+sleep 7
+echo 'testing is completed, please, do the sanity checks'
+sleep 10
+echo 'sanity checks completed and all looking great'
+echo 'Thank you all for joining, job well done'
+echo 'today is' `date`
