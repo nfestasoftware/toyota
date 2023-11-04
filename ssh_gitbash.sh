9d0c4f4e96e0645d631aeedbfc5809f00ad2c70a
festus nkengbeza <festusnkeng@yahoo.com>
2023-11-04 10:56:30 -0400

    add ssh gitbash testing file

---
+++ ssh_gitbash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'testing our ssh connection from gitbash'
+sleep 5
+echo 'testing in progress...!'
+sleep 5
+echo 'testing completed.'
+echo 'we have successfully completed gitbash ssh access to GitHub'
+echo today is `date`
