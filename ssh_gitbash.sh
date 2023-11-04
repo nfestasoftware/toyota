a3aa8c2e144a3ec85de2ea9b04abcd5d8b439272
nfestasoftware <nfestasoftware@gmail.com>
2023-11-04 11:49:23 -0400

    updated this file

--- ssh_gitbash.sh
+++ ssh_gitbash.sh
@@ -7,4 +7,5 @@ echo 'testing completed.'
 echo 'we have successfully completed gitbash ssh access to GitHub'
 echo 'Trust yoo all are excited completing git/github seession'
 echo 'Get ready as we will start seeing github intergrating with other tools'
+echo 'This will be experienced during our next session traing'
 echo today is `date`
